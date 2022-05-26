33d8e0ed5972c6eaebc9b5582d5f909f5c6d390c
Eddiesti <51298768+vivalafreak1@users.noreply.github.com>
2022-05-26 14:56:49 +0700

    Adding functional test to mathbasic.add, mathbasic.subtract

---
+++ src/createServer.test.js
@@ -0,0 +1,48 @@
+const createServer = require('./createServer');
+const MathBasic = require('./MathBasic');
+
+describe('A HTTP Server', () => {
+    describe('When GET /add', () => {
+        it('Should respond with a status code of 200 and the payload value is addition result of a and b correctly', async () => {
+            // Arrange
+            const a = 10;
+            const b = 20;
+            const spyAdd = jest.spyOn(MathBasic, 'add');
+            const server = createServer({ mathBasic: MathBasic });
+
+            // Action
+            const response = await server.inject({
+                method: 'GET',
+                url: `/add/${a}/${b}`,
+            });
+
+            // Assert
+            const responseJson = JSON.parse(response.payload);
+            expect(response.statusCode).toEqual(200);
+            expect(responseJson.value).toEqual(30);
+            expect(spyAdd).toBeCalledWith(a, b);
+        });
+    });
+
+    describe('When GET /subtract', () => {
+        it('Should respond with a status code of 200 and the payload value is subtraction result of a and b correctly', async () => {
+            // Arrange
+            const a = 12;
+            const b = 8;
+            const spySubtract = jest.spyOn(MathBasic, 'subtract');
+            const server = createServer({ mathBasic: MathBasic });
+
+            // Action
+            const response = await server.inject({
+                method: 'GET',
+                url: `/subtract/${a}/${b}`,
+            });
+
+            // Assert
+            const responseJson = JSON.parse(response.payload);
+            expect(response.statusCode).toEqual(200);
+            expect(responseJson.value).toEqual(4);
+            expect(spySubtract).toBeCalledWith(a, b);
+        });
+    });
+});
